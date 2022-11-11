0aa37e1710aae3235f14704c68fea0106a3a14a3
Nima Salem <nima@Nimas-MacBook-Air.local>
2022-11-11 12:22:46 +0330

    add sub-add regexes

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import re
+
+
 class Calculator:
 
     def __init__(self) -> None:
         pass
+
+
+add_regex = re.compile(r'(\d+) \+ (\d+)')
+sub_regex = re.compile(r'(\d+) - (\d+)')
 
 
 exit_command = 'Exit'
