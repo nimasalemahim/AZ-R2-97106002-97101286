1bba50e1f001fcffd4c62d92794c56f7dd8fcd8e
Nima Salem <nima@Nimas-MacBook-Air.local>
2022-11-11 12:38:12 +0330

    add function

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ class Calculator:
     def __init__(self) -> None:
         pass
 
+    def add(self, num1, num2):
+        return num1 + num2
+
 
 add_regex = re.compile(r'(\d+) \+ (\d+)')
 sub_regex = re.compile(r'(\d+) - (\d+)')
 mul_regex = re.compile(r'(\d+) \* (\d+)')
-div_regex = re.compile(r'(\d+) \% (\d+)')
+div_regex = re.compile(r'(\d+) / (\d+)')
 
 
 exit_command = 'Exit'
